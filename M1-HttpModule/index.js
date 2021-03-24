6d62ef65fed0b76c052a0cd2985825e9da224815
darwinchandra <darwin.chandra0@gmail.com>
2021-03-24 20:46:03 +0700

    latihan ubah port

--- M1-HttpModule/index.js
+++ M1-HttpModule/index.js
@@ -3,10 +3,13 @@ const http = require('http');
 //buat object server dengan listen port 3000
 http
   .createServer(function (req, res) {
+    //http Header
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
     //Respon ke browser / client
     res.write('<H1>Module HTTP Mobile and Web</H1>');
 
     //Respon diakhiri
     res.end();
   })
-  .listen(3000);
+  .listen(3400);
