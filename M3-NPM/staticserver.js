13b4ca0b5239f6045ecdc68353e7fcaa4cd736c9
darwinchandra <darwin.chandra0@gmail.com>
2021-04-17 21:28:51 +0700

    m4 stegah

--- M3-NPM/staticserver.js
+++ M3-NPM/staticserver.js
@@ -4,9 +4,7 @@ var logger = require('morgan');
 
 server.use(logger('dev'));
 server.use(express.static(__dirname + '/public'));
-server.get('/', (req, res) => {
-  res.send('<h1>abc</h1>');
-});
+
 server.listen(4000, function () {
   console.log('Server Static dijalankan');
 });
