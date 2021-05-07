286ecfe6425d4ff695c1a93160ecdc8be40fa351
darwinchandra <darwin.chandra0@gmail.com>
2021-05-07 21:33:53 +0700

    m5dan6

--- M5-QUERYPARAMS/index.js
+++ M5-QUERYPARAMS/index.js
@@ -1,29 +1,29 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-var bodyParser = require("body-parser");
+var bodyParser = require('body-parser');
 
 //https://github.com/expressjs/morgan
-var logger = require("morgan");
+var logger = require('morgan');
 
 //Module morgan merupakan modul untuk logger yang berfungsi untuk pencatatan tiap request ke server. Pencatatan ini atau istilahnya logging akan ditunjukkan di console terminal.
-app.use(logger("dev"));
+app.use(logger('dev'));
 
 //route dengan method get
-app.get("/api/:nim/:nama", function (req, res) {
+app.get('/api/:idpustakawan/:password', function (req, res) {
   res.statusCode = 200;
   //content-type pada expressjs
-  res.setHeader("Content-Type", "text/plain");
+  res.setHeader('Content-Type', 'text/plain');
   res.send(req.params);
 });
 
-app.get("/api/cari", function (req, res, next) {
-  var nama = req.query.nama;
-  console.log(`nama : ${nama}`);
-  var umur = req.query.umur;
-  console.log(`umur : ${umur}`);
-  res.send(umur);
+app.get('/api/cari', function (req, res, next) {
+  var idpustakawan = req.query.idpustakawan;
+  console.log(`Id Pustakawan : ${idpustakawan}`);
+  var password = req.query.password;
+  console.log(`password : ${password}`);
+  res.send(password);
 });
 
 app.listen(4000, function () {
-  console.log("Server run");
+  console.log('Server run');
 });
